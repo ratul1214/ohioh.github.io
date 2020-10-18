91c66f8095029f870f8c772d68930f41c10a15b8
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2020-10-18 23:09:19 +0200

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,93 +1,49 @@
-var deferredPrompt;
-const staticCacheName = 'pages-cache-v1';
+var cacheName = 'OHIOH-v1';
 
-const filesToCache = [
-  '/',
-  'index.html',
-  'pages/404.html'
-];
+var appShellFiles = [
+  '/index.html'
+]
 
 
-if (!window.Promise) {
-    window.Promise = Promise;
-}
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('flutter_service_worker.js')
-        .then(function () {
-            console.log('Service worker registered!');
-        })
-        .catch(function (err) {
-            console.log(err);
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
         });
-}
-
-window.addEventListener('beforeinstallprompt', function (event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
-});
-
-
-
-self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
-  event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
+      });
     })
   );
 });
 
 
-
-self.addEventListener('activate', event => {
-  console.log('Activating new service worker...');
-
-  const cacheWhitelist = [staticCacheName];
-
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-        .then(response => {
-          if (response.status === 404) {
-            return caches.match('pages/404.html');
-          }
-          return caches.open(staticCacheName)
-            .then(cache => {
-              cache.put(event.request.url, response.clone());
-              return response;
-            });
-        });
-    }).catch(error => {
-      console.log('Error, ', error);
-      return caches.match('pages/offline.html');
-    })
-  );
-});
-
-
-
