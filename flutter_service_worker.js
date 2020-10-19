472ca469f94fd6346982c69519d5d1afa5191c9a
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2020-10-19 16:46:26 +0200

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,6 +1,6 @@
 
-var CACHE_STATIC_NAME = 'static-v0';
-var CACHE_DYNAMIC_NAME = 'dynamic-v0';
+var CACHE_STATIC_NAME = 'static-v1';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
 self.addEventListener('install', function(event) {
   console.log('[OHIOH]: Installing ServiceWorker...',event);
@@ -36,30 +36,21 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('[OHIOH]👍 ServiceWorker is fetching.',event);
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-      caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                });
-            })
-            .catch(function(err) {
-              return caches.open(CACHE_STATIC_NAME)
-                .then(function (cache) {
-                  return cache.match('/offline.html');
-                  });
-            });
-        }
-      })
-  );
+      caches.open(CACHE_STATIC_NAME)
+          .then(function(cache) {
+              cache.match(event.request)
+                  .then( function(cacheResponse) {
+                      if(cacheResponse)
+                          return cacheResponse
+                      else
+                          return fetch(event.request)
+                              .then(function(networkResponse) {
+                                  cache.put(event.request, networkResponse.clone())
+                                  return networkResponse
+                              })
+                  })
+          })
+  )
 });
-
