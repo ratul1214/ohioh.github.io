0ff5f5a98068dee1b575c7a3982a139108c7357e
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2020-10-19 15:16:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,27 +17,41 @@
 
 var deferredPrompt; //before install needed
 
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-   .register('/flutter_service_worker.js')
-   .then(function() {
-   console.log("[OHIOH]: ServiceWorker is registered.")
-   });
- }
-
-
 function showAddToHomeScreen() { var a2hsBtn = document.querySelector(".ad2hs-prompt"); a2hsBtn.style.display = "block"; a2hsBtn.addEventListener("click", addToHomeScreen); }
 //function addToHomeScreen() { var a2hsBtn = document.querySelector(".ad2hs-prompt"); // hide our user interface that shows our A2HS button a2hsBtn.style.display = 'none'; // Show the prompt deferredPrompt.prompt(); // Wait for the user to respond to the prompt deferredPrompt.userChoice .then(function(choiceResult){ if (choiceResult.outcome === 'accepted') { console.log('User accepted the A2HS prompt'); } else { console.log('User dismissed the A2HS prompt'); } deferredPrompt = null; }); }
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/flutter_service_worker.js')
+    .then(function() {
+      console.log("[OHIOH]: ServiceWorker is registered.")
+    });
+}
 
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
 
 
 window.addEventListener('beforeinstallprompt', function (event) {
   console.log('[OHIOH]:👍 Prepaired to install...',event);
     // Prevent the mini-infobar from appearing on mobile
     // Prevent Chrome 67 and earlier from automatically showing the prompt
-  event.preventDefault();
+  //event.preventDefault();
     // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
     // Update UI notify the user they can install the PWA
@@ -47,23 +61,8 @@ window.addEventListener('beforeinstallprompt', function (event) {
   btnAdd.style.display = 'block';
   });
 
-  btnAdd.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
 
 window.addEventListener('appinstalled', (event) => {
   console.log('👍', 'appinstalled', event);
 });
+
