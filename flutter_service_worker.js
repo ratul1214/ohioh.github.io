68cb5ee69f62afe004b0c9bd1a7a10010121a18e
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2020-10-19 17:29:09 +0200

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,66 +1,93 @@
-var CACHE_STATIC_NAME = 'static-v1';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_NAME = 'vid-voter-v3'
+const URLS_CACHE_ONLY = [
+    // css and js
+    // font-awesome
 
-
-var STATIC_FILES = [
-  '/',
-  '/index.html',
-  '/offline.html'
 ];
 
+const URLS_OVER_NETWORK_WITH_CACHE_FALLBACK = [
+    "/index.html",
+    "/offline.html",
+];
 
-self.addEventListener('install', function(event) {
-  console.log('[OHIOH]: Installing ServiceWorker...',event);
-  event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-      .then(function(cache) {
-        console.log('[OHIOH]:👍 Precaching App Shell');
-        cache.addAll(STATIC_FILES);
-      })
-  )
-});
-
-
-self.addEventListener('activate', function(event) {
-  console.log('[OHIOH]:👍 ServiceWorker activated.',event);
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[OHIOH]:👍 Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-  // stabilize the sw
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function(err) {
-              return caches.open(CACHE_STATIC_NAME)
-                .then(function(cache) {
-                  return cache.match('/offline.html');
-                });
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(
+             URLS_CACHE_ONLY.concat(URLS_OVER_NETWORK_WITH_CACHE_FALLBACK)
+            );
+        }).catch((err) => {
+            console.error(err);
+            return new Promise((resolve, reject) => {
+                reject('ERROR: ' + err);
             });
-        }
-      })
-  );
+        })
+    );
 });
+
+self.addEventListener("fetch", function (event) {
+    const requestURL = new URL(event.request.url);
+
+    if (requestURL.pathname === '/') {
+      event.respondWith(getByNetworkFallingBackByCache("/index.html"));
+    } else if (URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.href) ||
+        URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.pathname)) {
+        event.respondWith(getByNetworkFallingBackByCache(event.request));
+    } else if (URLS_CACHE_ONLY.includes(requestURL.href) || 
+       URLS_CACHE_ONLY.includes(requestURL.pathname)) {
+        event.respondWith(getByCacheOnly(event.request));
+    }
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (CACHE_NAME !== cacheName && cacheName.startsWith("vid-voter")) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+/**
+ * 1. We fetch the request over the network
+ * 2. If successful we add the new response to the cache
+ * 3. If failed we return the result from the cache
+ *
+ * @param request
+ * @param showAlert
+ * @returns Promise
+ */
+const getByNetworkFallingBackByCache = (request, showAlert = false) => {
+    return caches.open(CACHE_NAME).then((cache) => {
+        return fetch(request).then((networkResponse) => {
+            cache.put(request, networkResponse.clone());
+            return networkResponse;
+        }).catch(() => {
+            if (showAlert) {
+                alert('You are in offline mode. The data may be outdated.')
+            }
+
+            return caches.match(request);
+        });
+    });
+};
+
+/**
+ * Get from cache
+ *
+ * @param request
+ * @returns Promise
+ */
+const getByCacheOnly = (request) => {
+    return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(request).then((response) => {
+            return response;
+        });
+    });
+};
 
