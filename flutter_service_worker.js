47b7d2970b4febdc07a648fa381c4ff63d07126c
Tjark Ziehm <63347689+ohioh@users.noreply.github.com>
2020-10-19 16:15:24 +0200

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,61 @@
 
+var CACHE_STATIC_NAME = 'static-v2';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
 self.addEventListener('install', function(event) {
   console.log('[OHIOH]: Installing ServiceWorker...',event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        console.log('[OHIOH]:👍 Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/main.dart',
+        ]);
+      })
+  )
 })
 
 
 self.addEventListener('activate', function(event) {
   console.log('[OHIOH]:👍 ServiceWorker activated.',event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[OHIOH]:👍 Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
   // stabilize the sw
   return self.clients.claim();
-})
+});
 
 self.addEventListener('fetch', function(event) {
   console.log('[OHIOH]👍 ServiceWorker is fetching.',event);
-  event.respondWith(fetch(event.request));
-})
+  event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                });
+            })
+            .catch(function(err) {
+
+            });
+        }
+      })
+  );
+});
+
